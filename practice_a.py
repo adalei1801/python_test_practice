a3fcdfd25f75df9afe9930d3626187816126c02a
Ashley <92755730+adalei1801@users.noreply.github.com>
2024-10-18 17:59:55 +0000

    practice a work 10/18

--- practice_a.py
+++ practice_a.py
@@ -19,31 +19,45 @@ The system should tell you if the guess is higher or lower than the actual numbe
 Reminder: Insert comments, both a multi-line before the code and in-line comments.
 """
 
+"""
+This program allows a uer four guesses at a number before 0 and 100
+"""
+
 from random import randint
+# Imports randint function from random module
 
 number = randint(0,100)
+# Assigns random number to variable
 attempts = 1
+
 
 print("")
 guess = int(input("Guess a number from 0-100: "))
 print("")
+# Allows user to input a guess 
 
 while attempts <= 4:
+    # While loop that iterates through guesses
     if guess == number:
         print("Correct!")
         break
+    # If statement that runs if user is correct and breaks the loop
     else:
         if guess > number:
             print("The answer is lower")
-            print("")
+            # If statement that tells the user that their guess is too larger
         else:
             print("The answer is higher")
-            print("")
+            # Else statement that tells the user their guess is below the number
         guess = int(input("Your guess was incorrect, try again: "))
+        # Gives the user another guess as the loop iterates
         print("")
         attempts += 1
+        # Increases attempt by one
         
     if attempts == 4:
         print(f"You are out of tries, the answer is {number}")
+        break
+    # If statement for the user running out of tries and breaks the loop
         
     
