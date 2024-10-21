93501ed5abfb07a7bb096f5bd2f88cb283174b56
Ashley King <ashleymking567@gmail.com>
2024-10-21 09:37:11 -0400

    add domain practice and practice b.3

---
+++ domain_practice/1.1_practice.py
@@ -0,0 +1,11 @@
+"""
+1.1	Evaluate an expression to identify the data type Python will assign to each variable - Identify str, int, float, and bool data types
+
+Identify the correct data type:
+“Hello”		3		4.7		true
+False		“78”		4/3		input()
++1E9		3.25*4
+
+
+"""
+
--- practice_b.py
+++ practice_b.py
@@ -7,16 +7,35 @@ B.2 Create a Mad Libs-style game, where the program asks the user for certain ty
 B.3 Create a game that will challenge you against the computer in a game of rock paper scissors. The user will input whether they choose rock paper or scissors and the computer will also choose one. The first to two will win the game
 """
 
-# B.1
+# B.3
 
+from random import randint
 
-def coin_count(change):
-   change = change // .25
-   print(change)
-   change = change // .1
-   print(change)
-   
+comp_choice = randint(1,3)
 
-change = float(input("How much change do you have? "))
-coin_count(change)
+if comp_choice == 1:
+    comp_choice = "rock"
+elif comp_choice == 2:
+    comp_choice = "paper"
+elif comp_choice == 3:
+    comp_choice = "scissors"
 
+print("Lets play rock, paper, scissors!\n")
+print("Options:\n1. Rock\n2. Paper\n3.Scissors")
+user_choice = int(input("Pick one(by number): "))
+
+if user_choice == 1:
+    user_choice = "rock"
+elif user_choice == 2:
+    user_choice = "paper"
+elif user_choice == 3:
+    user_choice = "scissors"
+
+print(f"{user_choice} v. {comp_choice}")
+
+if user_choice == comp_choice:
+    print("Tie!")
+elif user_choice == "rock" and comp_choice == "paper" or user_choice == "paper" and comp_choice == "scissors" or user_choice == "scissors" and comp_choice == "rock":
+    print("CPU won")
+else:
+    print("You won!")
