2aab221c5af21b074231f7ff470f7aaddba02fbe
Ashley King <ashleymking567@gmail.com>
2024-10-23 09:51:53 -0400

    additions to game

--- practice_b.py
+++ practice_b.py
@@ -20,22 +20,46 @@ elif comp_choice == 2:
 elif comp_choice == 3:
     comp_choice = "scissors"
 
-print("Lets play rock, paper, scissors!\n")
-print("Options:\n1. Rock\n2. Paper\n3.Scissors")
-user_choice = int(input("Pick one(by number): "))
+game = 0
+comp_wins = 0
+user_wins = 0
+ties = 0
 
-if user_choice == 1:
-    user_choice = "rock"
-elif user_choice == 2:
-    user_choice = "paper"
-elif user_choice == 3:
-    user_choice = "scissors"
+while True:
+    game += 1
+    print(f"Game {game}")
+    print("Lets play rock, paper, scissors!\n")
+    print("Options:\n1. Rock\n2. Paper\n3.Scissors")
+    user_choice = int(input("Pick one(by number): "))
 
-print(f"{user_choice} v. {comp_choice}")
+    if user_choice == 1:
+        user_choice = "rock"
+    elif user_choice == 2:
+        user_choice = "paper"
+    elif user_choice == 3:
+        user_choice = "scissors"
+    elif user_choice == 4:
+        print(f"You won {user_wins} times and the computer won {comp_wins}.\nYou tied {ties} times.")
+        if user_wins > comp_wins:
+            print("You won the most")
+        elif user_wins < comp_wins:
+            print("The computer won the most")
+        elif user_wins == comp_choice:
+            print("You tied in wins")
 
-if user_choice == comp_choice:
-    print("Tie!")
-elif user_choice == "rock" and comp_choice == "paper" or user_choice == "paper" and comp_choice == "scissors" or user_choice == "scissors" and comp_choice == "rock":
-    print("CPU won")
-else:
-    print("You won!")
+        if ties > user_wins and ties > comp_wins:
+            print("You tied the most")
+        
+        break
+
+    print(f"{user_choice} v. {comp_choice}")
+
+    if user_choice == comp_choice:
+        print("Tie!")
+        ties +=1
+    elif user_choice == "rock" and comp_choice == "paper" or user_choice == "paper" and comp_choice == "scissors" or user_choice == "scissors" and comp_choice == "rock":
+        print("CPU won")
+        comp_wins +=1
+    else:
+        print("You won!")
+        user_wins += 1
